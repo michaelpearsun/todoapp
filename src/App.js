d0ef95e6329a751643a28306f9eda9a21b953e36
Michael Pearson <michaelpearsun@gmail.com>
2020-09-28 13:13:37 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ya jerkle</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with imaginary boss',
+        completed: false
+      }
+    ]
+    }
+  render() {
+    return (
+      <div className="App">
+          <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
