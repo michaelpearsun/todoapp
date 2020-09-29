24db9fc6044041483febf31a7864450072a7694d
Michael Pearson <michaelpearsun@gmail.com>
2020-09-29 13:18:12 -0500

    About page and Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
@@ -45,24 +47,30 @@ class App extends Component {
 
   // Add Todo
   addTodo = (title) => {
-    const newTodo ={
+    const newTodo = {
       id: uuidv4(),
       title: title,
       completed: false
     }
-    this.setState({ todos: [...this.state.todos, newTodo]})
+    this.setState({ todos: [...this.state.todos, newTodo] })
   };
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />          
+          </div>
         </div>
-      </div>
-        
+      </Router>
     );
   }
 }
